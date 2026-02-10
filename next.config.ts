30a2d15e50e99ac10b36e5ef9a25206304ca5e8c
Igor-c17 <igordankesttrout7@gmail.com>
2026-02-10 16:42:27 -0300

    chore: stabilize build for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	reactCompiler: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.sanity.io",
-			},
-		],
-	},
+  reactCompiler: true,
+  poweredByHeader: false,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
